07073ab56d2485578a3b77018fe8f35bdf7be56a
Tomaso Cecchino <tomasocecchino@gmail.com>
2025-09-24 08:56:27 +0000

    Hai obbligatoriamente bisogno di aggiungere il plugin next-intl nel tuo

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 
 import type {NextConfig} from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -45,4 +48,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
