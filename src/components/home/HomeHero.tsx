ba02078dc224d25800f47e610bbeabc1139a85b7
Tomaso Cecchino <tomasocecchino@gmail.com>
2025-09-26 18:05:49 +0000

    Nella visione mobile si potrebbe spostare un po' più in basso il testo d

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -22,7 +22,7 @@ export function HomeHero() {
             </div>
             <div className="container z-10 h-full">
                 <div className="grid md:grid-cols-2 items-center h-full">
-                    <div className="py-16 md:py-24 text-left flex flex-col justify-between h-full pt-24 sm:pt-16">
+                    <div className="py-16 md:py-24 text-left flex flex-col justify-between h-full pt-48 sm:pt-16">
                         <div>
                             <h1 className="text-6xl sm:text-7xl font-headline tracking-tight text-white">
                                 <span className="font-normal">Giovanni</span>
