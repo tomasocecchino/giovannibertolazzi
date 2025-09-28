94a8566e4f99ccc98983debca331598c691f3fb3
Tomaso Cecchino <tomasocecchino@gmail.com>
2025-09-28 00:12:40 +0000

    Ora vorrei cambiare la favicon di firebase con questa: https://firebases

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -8,6 +8,9 @@ import { setRequestLocale } from "next-intl/server";
 export const metadata: Metadata = {
   title: "Giovanni Bertolazzi",
   description: "The official website of Giovanni Bertolazzi, Concert-Pianist",
+  icons: {
+    icon: 'https://firebasestorage.googleapis.com/v0/b/giovanni-bertolazzi.firebasestorage.app/o/GB.png?alt=media&token=7dd3a1e3-0244-455a-b48e-11a8e2a05a10',
+  },
 };
 
 type Props = {
