a9f990a665adf2162071e697ebe7b99479cf42ef
Tomaso Cecchino <tomasocecchino@gmail.com>
2025-09-28 01:13:48 +0000

    Installing nextjs adaptor 14.0.18 === Node.js - Npm (google.nodejs.npm@1

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,10 +3,9 @@
 
 import Error from 'next/error';
 
-// Render the default Next.js 404 page when a route
-// is requested that doesn't match the middleware and
-// therefore doesn't have a locale associated with it.
-
+// This component is not used for localized 404 pages.
+// It's a fallback for requests that don't have a locale.
+// The primary 404 page is in [locale]/not-found.tsx.
 export default function NotFound() {
   return (
     <html lang="en">
