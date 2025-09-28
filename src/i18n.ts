57f02db8479eb5363089ebc30b9f0d8fce18f89e
Tomaso Cecchino <tomasocecchino@gmail.com>
2025-09-28 00:58:58 +0000

    Continui a ostinarti a usare sempre la stessa struttura di codice, ma lo

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,11 @@
-import {getRequestConfig} from 'next-intl/server';
+import {getRequestConfig, getLocale} from 'next-intl/server';
 import {notFound} from 'next/navigation';
 import {locales} from './i18nConfig';
  
-export default getRequestConfig(async ({locale}) => {
+export default getRequestConfig(async () => {
+  // Obtain the locale from the request
+  const locale = await getLocale();
+ 
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound();
  
