5673039edcf6c56aa41835145e8ab7443be97571
Tomaso Cecchino <tomasocecchino@gmail.com>
2025-09-28 00:21:24 +0000

    Perchè dici che modifichi le cose e poi non lo fai... Ho appena cercato

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,10 @@
-import {getRequestConfig} from 'next-intl/server';
+import {getRequestConfig, getLocale} from 'next-intl/server';
 import {notFound} from 'next/navigation';
 import {locales} from './i18nConfig';
  
-export default getRequestConfig(async ({locale}) => {
+export default getRequestConfig(async () => {
   // Validate that the incoming `locale` parameter is valid
+  const locale = await getLocale();
   if (!locales.includes(locale as any)) notFound();
  
   return {
