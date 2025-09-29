984b0caeb7d5e03e286d7bfab272b6181ada76e5
Tomaso Cecchino <tomasocecchino@gmail.com>
2025-09-29 11:46:09 +0000

    la sitemap non si potrebbe caricare direttamente su https://giovannibert

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,8 @@
+
 import { MetadataRoute } from 'next'
  
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://www.giovannibertolazzi.com';
+  const baseUrl = 'https://giovannibertolazzi.com';
 
   const staticRoutes = [
     '/',
